8d37880391b9cae50b945781c4ea3feb0552c0aa
maren <jmcastri01@gmail.com>
2022-01-25 09:15:54 +0100

    first commit

---
+++ 1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "maren";
+            Console.WriteLine("Hola "+nombre);
+        }
+    }
+}
---
+++ 2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserta un número:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Tu número es: " + numero);
+        }
+    }
+}
