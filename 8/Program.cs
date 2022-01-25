5b47539d1e68fffa33c561860299e05bebe6955a
maren <jmcastri01@gmail.com>
2022-01-25 09:17:02 +0100

    first commit

---
+++ 8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca un numero:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                int mult = num * i;
+                Console.WriteLine(num + " x " + i + " = " + mult);
+            }
+
+        }
+    }
+}
---
+++ 9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca unos segundos:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            int horas = numero / 3600;
+            int minutos = (numero % 3600) / 60;
+            int segundos = minutos % 60;
+            Console.WriteLine("Tus " + numero + " segundos son: " + horas + " horas " + minutos + " minutos " + segundos + " segundos");
+        }
+    }
+}
